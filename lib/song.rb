51bead894aa1ddfa838064cd3e97431eccd80fd6
Tyler Firth <tyfirth@gmail.com>
2020-05-24 01:38:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,11 @@ class Song
   def genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] = genre 
+      if genre_count[genre] 
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1 
+      end
   end
   
   def self.artists
