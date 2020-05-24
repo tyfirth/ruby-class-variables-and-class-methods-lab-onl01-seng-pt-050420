9c99174beff34c0cb5918d86b635455ba1aa07fe
Tyler Firth <tyfirth@gmail.com>
2020-05-24 01:44:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,18 @@ class Song
     @@artists.uniq
   end
   
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
+  end
+  
   
   def initialize(name, artist, genre)
     @@count += 1 
