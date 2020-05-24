e0e8b90b4066d3b41a2e3a3d8b9a305451979449
Tyler Firth <tyfirth@gmail.com>
2020-05-24 01:27:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  
+  @@genres = []
+  
+  @@artists = []
+  
+    def self.count
+   @@count
+  end
+  
+  
+  
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre 
+    @@artists << artist
+  end
+  
+
+
+end
